93a8cdd80eca3093c28997fe6c1f9ac6312904e9
welynton oliveira <oliveirawelynton@gmail.com>
2020-07-14 22:04:23 -0300

    Links navegaveis

--- js/fuction.js
+++ js/fuction.js
@@ -32,8 +32,65 @@ document.addEventListener('DOMContentLoaded', function() {
       left: 'prev,next today',
       center: 'title',
       right: 'timeGridDay,dayGridWeek,dayGridMonth,listWeek'
-    }
-
+    }, 
+    events: [
+      {
+        id: 'consulta-confirmada',
+        title: 'Consulta Confirmada',
+        start: '2020-07-15T17:00',
+        end: '2020-07-15T17:30',
+        url: 'consulta.html',
+        backgroundColor: '#00C853',
+        borderColor: '#00C853',
+        extendedProps: {
+          médico: 'João da Silva'
+        },
+      }, 
+      {
+        id: 'consulta-confirmada',
+        title: 'Consulta Confirmada',
+        start: '2020-07-15T17:00',
+        end: '2020-07-15T17:30',
+        url: 'consulta.html',
+        backgroundColor: '#00C853',
+        borderColor: '#00C853',
+      }, 
+      {
+        id: 'consulta-ausencia',
+        title: 'Ausência',
+        start: '2020-07-15T17:30',
+        end: '2020-07-15T18:00',
+        url: 'consulta.html',
+        backgroundColor: '#d50000',
+        borderColor: '#d50000',
+      }, 
+      {
+        id: 'consulta-cancelada',
+        title: 'Consulta Cancelada',
+        start: '2020-07-15T18:00',
+        end: '2020-07-15T18:30',
+        url: 'consulta.html',
+        backgroundColor: '#d50000',
+        borderColor: '#d50000',
+      }, 
+      {
+        id: 'consulta-agendada',
+        title: 'Consulta Agendada',
+        start: '2020-07-15T18:30',
+        end: '2020-07-15T19:00',
+        url: 'consulta.html',
+        backgroundColor: '#2962FF',
+        borderColor: '#2962FF',
+      }, 
+      {
+        id: 'horario-reservado',
+        title: 'Horário Reservado',
+        start: '2020-07-15T19:15',
+        end: '2020-07-15T20:15',
+        backgroundColor: '#616161',
+        borderColor: '#616161',
+      },
+    ]
   });
   calendar.render();
 });
